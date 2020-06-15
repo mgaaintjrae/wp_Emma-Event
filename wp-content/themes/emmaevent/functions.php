bd3630510a53fb5ed3ed997fba13236bca586a60
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-15 14:17:47 +0200

    sidebar page event

--- wp-content/themes/emmaevent/functions.php
+++ wp-content/themes/emmaevent/functions.php
@@ -3,6 +3,7 @@ require_once('inc/supports.php');
 require_once('inc/assets.php');
 require_once('inc/apparence.php');
 require_once('inc/menus.php');
+require_once('inc/images.php');
 
 
 function emmaevent_icon(string $name): string {
@@ -27,12 +28,7 @@ function emmaevent_add_normalize_CSS()
 
 
 
-// Définir la taille des images mises en avant
-set_post_thumbnail_size(2000, 400, true);
 
-// Définir d'autres tailles d'images
-add_image_size('products', 800, 600, false);
-add_image_size('square', 256, 256, false);
 
 
 // Custom Post Type Events
