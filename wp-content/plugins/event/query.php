518828cdb1c7f1a70ea0a18494725999b98ead7b
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-17 12:07:34 +0200

    add post-type in url

--- wp-content/plugins/event/query.php
+++ wp-content/plugins/event/query.php
@@ -2,5 +2,5 @@
 
 // Réécriture d'url
 add_action('init', function () {  
-  add_rewrite_rule(_x('events', 'URL', 'event') . '/?$', 'index.php?event_category=$matches[1]', 'top');
+  add_rewrite_rule(_x('events', 'URL', 'event') . '/?$', 'index.php?post_type=events&event_category=$matches[1]', 'top');
 });
