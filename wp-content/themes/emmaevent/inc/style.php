0735f8ae1e02ef937ac7fcdf4065b8adeb1c400b
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-17 12:06:55 +0200

    modif pour le current_page du menu

--- wp-content/themes/emmaevent/inc/style.php
+++ wp-content/themes/emmaevent/inc/style.php
@@ -2,3 +2,12 @@
 add_filter('next_posts_link_attributes', function (string $attrs): string {
   return $attrs . ' class="btn"';
 });
+
+add_filter('nav_menu_css_class', function (array $classes): array {
+  if(is_singular('event') || is_post_type_archive('event') || is_tax('events')) {
+    $classes = array_filter($classes, function (string $class) {
+      return $class !== 'current_page_parent';
+    });
+  }
+  return $classes;
+});
