907d42bda111c78af22a83cbcecd606ea82b1743
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-17 09:49:55 +0200

    add page event : listing event

--- wp-content/themes/emmaevent/archive-event.php
+++ wp-content/themes/emmaevent/archive-event.php
@@ -1,10 +1,10 @@
 <?php get_header() ?>
 
-<div class="container page-properties">
+<div class="container page-events">
     <div class="search-form">
-  <h1 class="search-form__title">Agence immo Montpellier</h1>
-  <p>Retrouver tous nos biens sur le secteur de <strong>Montpellier</strong></p>
-  <hr>
+  <h1 class="search-form__title">Evénements</h1>
+  <p>Retrouver tous mes événements en <strong>Ile de France</strong></p>
+  <!--<hr>
   <form action="listing.html" class="search-form__form">
     <div class="search-form__checkbox">
       <div class="form-check form-check-inline">
@@ -36,30 +36,26 @@
       <label for="rooms">Pièces</label>
     </div>
     <button type="submit" class="btn btn-filled">Rechercher</button>
-  </form>
+  </form>-->
 </div>
 
 <?php if (have_posts()) : ?>
       <?php while (have_posts()) : the_post(); ?>
       <a class="event " href="<?php the_permalink() ?>" title="<?= esc_attr(get_the_title()) ?>">
   <div class="event__image">
-    <?php the_post_thumbnail('event-thumbnail') ?>
-      <img src="https://i.picsum.photos/id/30/385/220.jpg" alt="">
-    
-  </div>
-  <!--<div class="event__body">
-    <div class="event__location">34000 Montpellier</div>
-    <h3 class="event__title">Maison 4 pièce(s) - 10m²</h3>
-    <div class="event__price">45 000 €</div>-->
-  </div>
-</a>
+    <?php the_post_thumbnail('event-thumbnail') ?>        
+  </a>
 <?php endwhile; ?>
 <?php endif; ?> 
-    
+
   </div>
 
+<?php if(get_query_var('paged', 1) > 1): ?>
+<?= emmaevent_paginate() ?>
+<?php elseif($nextPostLink = next_posts_link(__('More events +', 'emmaevent'))): ?>
   <div class="pagination">
-    <a href="#" class="btn">Voir plus d'événements +</a>
+  <?= $nextPostLink  ?>    
   </div>
+<?php endif; ?>  
   
 <?php get_footer() ?>
