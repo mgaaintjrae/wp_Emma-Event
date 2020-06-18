8674702a66e2199464dac5d770a8841d689e08bc
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-18 11:22:31 +0200

    add size thumbnail avant regenerate

--- wp-content/themes/emmaevent/inc/images.php
+++ wp-content/themes/emmaevent/inc/images.php
@@ -3,6 +3,7 @@ add_action('after_setup_theme', function () {
   // Définir la taille des images mises en avant
   set_post_thumbnail_size(375,375,true);
   add_image_size('event-carousel', 800, 600, true);
+  add_image_size('event-thumbnail', 575, 575, true);
   add_image_size('event-thumbnail-large', 875, 220, true);  
   
 });
