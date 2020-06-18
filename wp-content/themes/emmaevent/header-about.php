a735df001bceb6c4f9bf3f8ff807970334a1a432
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-18 22:08:37 +0200

    add pages contact/about + 2 news header

---
+++ wp-content/themes/emmaevent/header-about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+  <header class="home">    
+
+    <div class="menu__area">
+      <div class="grid">
+
+        <button class="nav__burger">
+          <span></span>
+        </button>
+
+        <div class="position__left__logo">
+          <a href="<?= home_url('/') ?>" class="nav__logo" title="<?= __('Homepage', 'emmaevent') ?>">
+            <img src="<?= get_theme_mod('logo header') ?>" alt="logo">
+          </a>
+        </div>
+
+        <div class="position__center">
+          <nav class="nav">
+            <?php
+            wp_nav_menu([
+              'theme_location' => 'header',
+              'container' => false,
+              'menu_class' => 'nav__menu'
+            ]);
+            ?>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <div class="header__title">
+      <h1>A propos</h1>
+      <img src="assets/img/divider_coeur.svg" alt="divider coeur white">
+    </div>
+    <div class="header__content">
+      <p>Laissez votre entreprise se développer grâce à ce thème étonnant</p>
+    </div>
+
+  </header>
---
+++ wp-content/themes/emmaevent/page-me-contacter.php
@@ -0,0 +1,28 @@
+<?php get_header('contact'); ?>
+
+<main class="sections">
+    <!-- Contact -->
+    <section>
+      <div class="container">
+        <div class="company">          
+          <div class="company__title">          
+          <?php the_post_thumbnail('event-thumbnail'); ?>          
+          <h2 class="page-title">
+            <?php the_title(); ?>
+          </h2>
+        </div>
+      </div>          
+    </section> 
+  
+    <section>
+      <div class="page-sidebar">        
+        <div class="news-list">
+          <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>          
+            <?php the_content(); ?>
+          <?php endwhile; endif; ?>
+        </div>     
+      </div>
+    </section>
+</main>
+<?php get_footer(); ?>
