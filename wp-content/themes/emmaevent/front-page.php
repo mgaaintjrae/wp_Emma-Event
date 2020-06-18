5946b27bede1883805235d3df07ac8856b74753b
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-18 11:25:08 +0200

    dynamisation accueil

--- wp-content/themes/emmaevent/front-page.php
+++ wp-content/themes/emmaevent/front-page.php
@@ -1,70 +1,46 @@
 <?php get_header(); ?>
 
+<?php while(have_posts()): the_post() ?>
+
   <main class="sections">
     <!-- Company presentation -->
     <section>
       <div class="container">
-        <div class="company">
-          <img src="assets/img/courone_fleurs.png" alt="couronne de fleurs" class="crown__flowers">
-          <div class="company__title">
-            <h2>Présentation</h2>
-            <img src="assets/img/divider_trefle.svg" alt="divider trèfle">
-          </div>
-
-          <p>nulla consectetur ut dapibus euismod quis, cillum ac eros cillum et velit fermentum. enim eget ut aliquip
-            nisl, mollis neque. scelerisque elit, quis malesuada gravida mollis. nulla consectetur ut dapibus euismod
-            quis, cillum ac eros cillum et velit fermentum. enim eget ut aliquip nisl, mollis neque. scelerisque elit,
-            quis malesuada gravida mollis,
-            nulla consectetur ut dapibus euismod quis, cillum ac eros cillum et velit fermentum. enim eget ut aliquip
-            nisl, mollis neque. scelerisque elit, quis malesuada gravida mollis</p>
-          <p>nulla consectetur ut dapibus euismod quis, cillum ac eros cillum et velit fermentum. enim eget ut aliquip
-            nisl, mollis neque. scelerisque elit, quis malesuada gravida mollis. nulla consectetur ut dapibus euismod
-            quis, cillum ac eros cillum et velit fermentum. enim eget ut aliquip nisl, mollis neque. scelerisque elit,
-            quis malesuada gravida mollis,
-            nulla consectetur ut dapibus euismod quis, cillum ac eros cillum et velit fermentum. enim eget ut aliquip
-            nisl, mollis neque. scelerisque elit, quis malesuada gravida mollis</p>
-        </div>
+        <div class="company">          
+          <div class="company__title">          
+          <?php the_post_thumbnail('event-thumbnail'); ?>          
+          <?php the_content(); ?>            
+          </div>          
     </section>
 
     <!-- Events -->
+    <?php if(have_rows('recent_events')): while(have_rows('recent_events')): the_row() ?>
     <section class="container__event">
       <div class="events">
         <div class="event__title">
-          <h2>Evénements</h2>
-          <img src="assets/img/divider_trefle.svg" alt="divider trèfle">
-        </div>
-
-
+          <h2><?php the_sub_field('title') ?></h2>
+          <?php the_sub_field('image') ?>
+          <img src="<?= get_field('divider_trefle'); ?>" alt="divider trèfle">          
+        </div>        
+        <?php the_sub_field('description') ?>      
+        
         <div class="events__grid">
-          <a class="event " href="#" title="Organisation de mariage">
-            <div class="event__image">
-              <img src="https://picsum.photos/475.jpg" alt="">
-            </div>
-            <div class="event__body">
-              <h3 class="event__category">Organisation de mariage</h3>
-            </div>
-          </a>
-
-          <a class="event " href="#" title="Evénements privés">
-            <div class="event__image">
-              <img src="https://picsum.photos/475.jpg" alt="">
-            </div>
-            <div class="event__body">
-              <h3 class="event__category">Evénements privés</h3>
-            </div>
-          </a>
-
-          <a class="event " href="#" title="Evénements professionels">
-            <div class="event__image">
-              <img src="https://picsum.photos/475.jpg" alt="">
-            </div>
-            <div class="event__body">
-              <h3 class="event__category">Evénements professionels</h3>
-            </div>
-          </a>
+        <?php
+        $query = new WP_Query([
+          'post_type' => 'event',
+          'posts_per_page' => 3
+        ]);        
+        
+        while($query->have_posts()){
+          $query->the_post();
+          get_template_part('template-parts/event');
+        }
+        wp_reset_postdata(); 
+        ?>     
         </div>
       </div>
     </section>
+    <?php endwhile; endif; ?>
 
 
     <!-- <section class="container quote">
@@ -92,4 +68,5 @@
     <?php echo do_shortcode('[masterslider id="1"]'); ?>
   </main>
   
+<?php endwhile; ?>
 <?php get_footer(); ?>
