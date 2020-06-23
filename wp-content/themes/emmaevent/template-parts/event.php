ef46d9819e43382a6cd397d945106c1f3fe22d19
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-23 12:15:45 +0200

    hover effect thumbnail events

--- wp-content/themes/emmaevent/template-parts/event.php
+++ wp-content/themes/emmaevent/template-parts/event.php
@@ -1,13 +1,21 @@
-<a class="event " href="<?php the_permalink() ?>" title="<?= esc_attr(get_the_title()) ?>">
-    <div class="event__image">
-        <?php the_post_thumbnail('event-thumbnail') ?>
-    </div>
-    <div class="event__body">
-        <?php $type = the_field('type');
+<div class="content">
+    <a class="event " href="<?php the_permalink() ?>" title="<?= esc_attr(get_the_title()) ?>">
+
+        <div class="event__image content-image">
+            <div class="content-overlay"></div>
+            <figure><?php the_post_thumbnail('event-thumbnail') ?></figure>
+        </div>
+        <div class="content-details fadeIn-bottom">
+            <div class="event__body content-title">
+                <?php $type = the_field('type');
                     if( $type ):?>
-        <?php foreach( $types as $type): ?>
-        <h3 class="event__type"><?= $type ?></h3>
-        <?php endforeach; ?>
-        <?php endif; ?>
-    </div>
-</a>
+                <?php foreach( $types as $type): ?>
+                <h3 class="event__type">
+                    <?php $type ?>
+                </h3>
+                <?php endforeach; ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </a>
+</div>
