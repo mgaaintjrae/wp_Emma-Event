b4fc1bb36ed41f2dd584156f8b05b8716ec0b302
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-29 22:21:31 +0200

    add link share RS

---
+++ wp-content/themes/emmaevent/template-parts/share.php
@@ -0,0 +1,35 @@
+<?php
+$lien = get_permalink();
+$titre = strip_tags(get_the_title());
+// $urlImage = the_post_thumbnail_url();
+?>
+
+<div class="event__share">
+    <h5 class="title-secondary">Partagez cet article !</h5>
+    <p>Cet article vous a plu ? N'hésitez pas à le partager !</p>
+
+    <a href="https://www.facebook.com/sharer.php?u=<?= $lien ?>&t=<?= $titre ?>" target="_blank"
+        class="btn-social btn-facebook" title="sur Facebook"
+        onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;"><i
+            class="fa fa-facebook"></i></a>
+
+    <!-- <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-instagram"
+        title="sur Instagram"
+        onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;"><i
+            class="fa fa-instagram"></i></a> -->
+
+    <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?= $lien ?>&title=<?= $titre ?>" target="_blank"
+        class="btn-social btn-linkedin" title="sur LinkedIn"
+        onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;"><i
+            class="fa fa-linkedin"></i></a>
+
+    <a href="https://twitter.com/share?url=<?= $lien ?>text=<?= $titre ?>&via=VOTRE-NOM-TWITTER" target="_blank"
+        class="btn-social btn-twitter"
+        onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=500,width=700');return false;"><i
+            class="fa fa-twitter" title="sur Twitter"></i></a>
+
+    <a href="mailto:info@example.com?&subject=&body=http://emma-event.fr" target="_blank" class="btn-social btn-email"
+        title="par mail"><i class="fa fa-envelope"></i></a>
+
+
+</div>
