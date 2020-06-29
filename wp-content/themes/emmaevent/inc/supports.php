00b7c96c701e12aabc6bd8c7577af73d1828c3ce
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-06-29 22:19:10 +0200

    i18n thème poedit

--- wp-content/themes/emmaevent/inc/supports.php
+++ wp-content/themes/emmaevent/inc/supports.php
@@ -1,11 +1,12 @@
 <?php
-defined('ABSPATH') or die('');
+defined('ABSPATH') or die('rien à voir');
 
 add_action('after_setup_theme', function () {
   add_theme_support('title-tag');
   add_theme_support('menus');
   add_theme_support('html5');
-  add_theme_support('post-thumbnails');
+  add_theme_support('post-thumbnails');  
+load_theme_textdomain('emmaevent', get_template_directory() . '/languages');
 });
 
 /** Upload SVG medias */
