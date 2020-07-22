d9d3e550d5d96c38409af475a71cff7bd2504760
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-07-22 21:10:59 +0200

    replace last event by prestations section

--- wp-content/themes/emmaevent/front-page.php
+++ wp-content/themes/emmaevent/front-page.php
@@ -32,7 +32,62 @@
 
 
     <!-- Events -->
-    <?php if (have_rows('recent_events')) : while (have_rows('recent_events')) : the_row() ?>
+    <?php //if (have_rows('recent_events')) : while (have_rows('recent_events')) : the_row() ?>
+    <!--<section class="container__event"> -->
+
+    <!-- Fantasy 
+    <div class="fantasy">
+        <div class="deco__left">
+            <img src="<?= get_stylesheet_directory_uri();?>/assets/img/branche_feuilles_left.png" alt="branche feuilles"
+                width="545" height="442">
+        </div>
+        <div class="deco__right">
+            <img src="<?= get_stylesheet_directory_uri();?>/assets/img/branche_feuilles_right.png"
+                alt="branche feuilles" width="545" height="442">
+        </div>
+    </div>
+     Fantasy end -->
+
+    <!--<div class="event">
+        <div class="event__title">
+            <h2><?php //the_sub_field('title') ?></h2>
+            <?php //the_sub_field('image') ?>
+            <svg class="icon">
+                <use xlink:href="<?= get_stylesheet_directory_uri();?>/assets/img/svg/sprite.svg#divider_trefle">
+                </use>
+            </svg>
+        </div>
+        <div class="event__content">
+            <div class="fantasy-2"></div>
+            <?php //the_sub_field('description') ?>
+        </div>
+
+        <div class="events__grid">
+            <?php
+							// $query = new WP_Query([
+							// 	'post_type' => 'event',
+							// 	'posts_per_page' => 3
+							// ]);
+
+            //   if (have_posts()) : ?>
+            <?php //while ($query->have_posts()) : $query->the_post(); ?>
+            <?php //get_template_part('template-parts/event'); ?>
+            <?php //wp_reset_postdata(); ?>
+            <?php //endwhile; endif; ?>
+        </div>
+        </div>
+
+        <div class="events__link">
+            <a href="<?= get_post_type_archive_link( 'event' ); ?>" class="btn">Voir tous les événements</a>
+        </div>
+
+        </section>-->
+    <!-- Events end -->
+
+    <?php //endwhile; ?>
+    <?php //endif; ?>
+
+    <!-- Prestations -->
     <section class="container__event">
 
         <!-- Fantasy -->
@@ -46,45 +101,106 @@
                     alt="branche feuilles" width="545" height="442">
             </div>
         </div>
+        <!-- Fantasy end -->
 
-        <div class="event">
-            <div class="event__title">
-                <h2><?php the_sub_field('title') ?></h2>
-                <?php the_sub_field('image') ?>
-                <svg class="icon">
-                    <use xlink:href="<?= get_stylesheet_directory_uri();?>/assets/img/svg/sprite.svg#divider_trefle">
-                    </use>
-                </svg>
-            </div>
-            <div class="event__content">
-                <div class="fantasy-2"></div>
-                <?php the_sub_field('description') ?>
-            </div>
-
-            <div class="events__grid">
-                <?php
-							$query = new WP_Query([
-								'post_type' => 'event',
-								'posts_per_page' => 3
-							]);
-
-              if (have_posts()) : ?>
-                <?php while ($query->have_posts()) : $query->the_post(); ?>
-                <?php get_template_part('template-parts/event'); ?>
-                <?php wp_reset_postdata(); ?>
-                <?php endwhile; endif; ?>
-            </div>
+        <div class="presta__title">
+            <h2><?php the_sub_field('title') ?></h2>
+            <?php the_sub_field('image') ?>
+            <svg class="icon">
+                <use xlink:href="<?= get_stylesheet_directory_uri();?>/assets/img/svg/sprite.svg#divider_trefle">
+                </use>
+            </svg>
         </div>
 
-        <div class="events__link">
-            <a href="<?= get_post_type_archive_link( 'event' ); ?>" class="btn">Voir tous les événements</a>
+        <div class="presta__content">
+            <?php the_sub_field('description') ?>
         </div>
 
+        <div class="presta__grid">
 
+            <div class="hover09 column">
+                <div>
+                    <figure><img src="https://picsum.photos/300/200?image=244" /></figure>
+                    <span>
+                        <p>
+                            AVANT :
+                            Rendez-vous de présentation et étude des besoins
+                            Recherche et sélection des prestataires
+                            Accompagnement et conseil dans l’organisation
+                            Organisation des visites (lieux, traiteur, DJ…)
+                            Scénographie (invitations, thème, papeterie, art floral, mise en lumière…)
+                            Mise en place des outils de gestion (retroplanning, fiche prestataires, dossier client…)
+                            PENDANT :
+                            Brief final
+                            Accueil et gestion sur place (invités, prestataires)
+                            Mise en place et coordination de l’évènement
+                            Gestions des imprévus éventuels
+                            APRES :
+                            Finalisation des prestations (Photographe, vidéaste)
+                            Retour de noce (Brunch, petit déjeuner, food truck, BBQ…)
+                            Gestion des remerciements
 
+                            Tarif : à partir de 3000€
+                        </p>
+                    </span>
+                </div>
+                <div>
+                    <figure><img src="https://picsum.photos/300/200?image=1024" /></figure>
+                    <span>
+                        <p>AVANT :
+                            Rendez-vous de présentation et étude des besoins
+                            Recherche et sélection des prestataires manquants
+                            Conseil et finalisation de l’organisation de votre évènement
+                            Mise en place des outils de gestion (retroplanning, fiche prestataires, dossier client…)
+                            PENDANT :
+                            Brief final
+                            Accueil et gestion sur place (invités, prestataires)
+                            Mise en place et coordination de l’évènement
+                            Gestions des éventuels imprévus
+                            APRES : (en option)
+                            Finalisation des prestations (Photographe, vidéaste)
+                            Retour de noce (Brunch, petit déjeuner, food truck, BBQ…)
+                            Gestion des remerciements
+
+                            Tarif : à partir de 2000€
+                        </p>
+                    </span>
+                </div>
+                <div>
+                    <figure><img src="https://picsum.photos/300/200?image=611" /></figure>
+                    <span>
+                        <p>
+                            JOUR J :
+                            Cette formule s’adresse à ceux qui ont juste une envie… savourer leur grand jour, profiter
+                            de
+                            l’instant présent et se laisser aller. Nous coordonnons tout pour que l’imprévu n’est pas sa
+                            place dans votre évènement
+                            AVANT :
+                            Rendez-vous avec les mariés
+                            Liste, rendez-vous téléphonique et brief avec les prestataires
+                            Mise en place d’un rétroplanning
+                            PENDANT :
+                            Brief final
+                            Accueil et gestion sur place (invités, prestataires)
+                            Mise en place et coordination de l’évènement
+                            Gestions des éventuels imprévus
+
+                            Tarif : 1300€
+                        </p>
+                    </span>
+                </div>
+                <div>
+                    <figure><img src="https://picsum.photos/300/200?image=612" /></figure>
+                    <span>
+                        <p>
+                            A LA CARTE
+                        </p>
+                    </span>
+                </div>
+            </div>
+        </div>
     </section>
-    <?php endwhile; ?>
-    <?php endif; ?>
+    <!-- Prestations end -->
 
     <!-- <section class=" container quote">
             <div class="quote__title">Ce que pensent mess clients</div>
@@ -108,6 +224,8 @@
             </a>
     </section> -->
 
+
+
     <!-- Slider -->
     <section>
         <div class="slider__container">
@@ -122,6 +240,7 @@
             </div>
         </div>
     </section>
+    <!-- Slider end -->
 
 </main>
 
