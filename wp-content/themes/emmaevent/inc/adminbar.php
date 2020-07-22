a2d4fb97eeb21617685fe9f71cea435758dd6a15
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-07-22 21:09:58 +0200

    adminbar none

---
+++ wp-content/themes/emmaevent/inc/adminbar.php
@@ -0,0 +1,10 @@
+<?php
+
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+show_admin_bar(false);
+}
+}
+
+add_filter('show_admin_bar', '__return_false');
