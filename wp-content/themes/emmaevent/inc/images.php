1f76a36937581a3fe16990cc3350d91e2299515d
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-07-27 16:03:44 +0200

    add image size

--- wp-content/themes/emmaevent/inc/images.php
+++ wp-content/themes/emmaevent/inc/images.php
@@ -2,17 +2,9 @@
 add_action('after_setup_theme', function () {
   // Définir la taille des images mises en avant
   set_post_thumbnail_size(375,375,true);
+  add_image_size('service-thumbnail', 300, 400, true);  
   add_image_size('event-carousel', 800, 600, true);
-  add_image_size('event-thumbnail', 575, 575, true);
+  add_image_size('event-thumbnail', 575, 575, true);  
   add_image_size('event-thumbnail-large', 1200, 320, true);  
   
 });
-
-
-
-
-
-
-// Définir d'autres tailles d'images
-// add_image_size('products', 800, 600, false);
-// add_image_size('square', 256, 256, false);
