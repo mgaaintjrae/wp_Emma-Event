fdf02cfd7b021ff6b32ac1d889bb879b7990ea0d
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-07-29 13:31:15 +0200

    add excerpt gestion admin

--- wp-content/themes/emmaevent/inc/supports.php
+++ wp-content/themes/emmaevent/inc/supports.php
@@ -5,8 +5,9 @@ add_action('after_setup_theme', function () {
   add_theme_support('title-tag');
   add_theme_support('menus');
   add_theme_support('html5');
-  add_theme_support('post-thumbnails');  
-load_theme_textdomain('emmaevent', get_template_directory() . '/languages');
+  add_theme_support('post-thumbnails'); 
+  add_post_type_support('page', 'excerpt'); 
+  load_theme_textdomain('emmaevent', get_template_directory() . '/languages');
 });
 
 /** Upload SVG medias */
