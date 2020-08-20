7737c7b6e54d9b5b7193bb6cd6352f1a4bada2ac
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-08-20 17:11:53 +0200

    desactivate MaJ plugin Event

---
+++ wp-content/themes/emmaevent/inc/plugin.php
@@ -0,0 +1,7 @@
+<?php
+// desactivate update plugin Event
+function stop_plugin_update( $value ) {
+    unset( $value->response['event/event.php'] );
+    return $value;
+   }
+   add_filter( 'site_transient_update_plugins', 'stop_plugin_update' );
