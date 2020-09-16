97d2a572e37a1f1283fc8ba0246386d3c34b4711
mgaaintjrae <pr.priscilla.roy@gmail.com>
2020-09-16 14:37:10 +0200

    modif front-page h2 + debug css

--- wp-content/themes/emmaevent/assets/js/custom.js
+++ wp-content/themes/emmaevent/assets/js/custom.js
@@ -1,55 +1,47 @@
 jQuery(document).ready(function (jQuery) {
-    'use strict';
+  "use strict";
 
-    var wind = jQuery(window);
+  var wind = jQuery(window);
 
-    wind.on('load', function () {
-        // Preloader
-        jQuery('.loading').fadeOut(500);
-    });
+  wind.on("load", function () {
+    // Preloader
+    jQuery(".loading").fadeOut(500);
+  });
 
-    // button scroll to top
-    jQuery('.button-top').click(function () {
-        jQuery('html,body').animate({ scrollTop: 0 }, 'slow');
-    });
+  // button scroll to top
+  jQuery(".button-top").click(function () {
+    jQuery("html,body").animate({ scrollTop: 0 }, "slow");
+  });
 
-    jQuery(window).scroll(function () {
-        // If on top fade the bouton out, else fade it in
-        if (jQuery(window).scrollTop() == 0) jQuery('.button-top').fadeOut();
-        else jQuery('.button-top').fadeIn();
-    });
-
-
+  jQuery(window).scroll(function () {
+    // If on top fade the bouton out, else fade it in
+    if (jQuery(window).scrollTop() == 0) jQuery(".button-top").fadeOut();
+    else jQuery(".button-top").fadeIn();
+  });
 });
 
-
 // Sticky menu
-document.addEventListener('DOMContentLoaded',function(){
-    'use strict';
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
+document.addEventListener("DOMContentLoaded", function () {
+  "use strict";
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
     var currentScrollpos = window.pageYOffset;
     if (prevScrollpos > currentScrollpos) {
-        document.querySelector('.navhome').style.top = '0px';
-        document.querySelector('.navhome').style.zIndex = 1;              
+      document.querySelector(".navhome").style.top = "0px";
+      document.querySelector(".navhome").style.zIndex = 1;
     } else {
-        document.querySelector('.navhome').style.top = '-190px';
-        document.querySelector('.navhome').classList.add('sticky');
+      document.querySelector(".navhome").style.top = "-190px";
+      document.querySelector(".navhome").classList.add("sticky");
     }
 
     prevScrollpos = currentScrollpos;
-};
-
+  };
 });
 
-
-// Sub menu
-document.addEventListener('DOMContentLoaded',function(){
-document.querySelector('.sub-menu').classList.add('height');
+document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
+  // Sub menu
+  document.querySelector(".sub-menu").classList.add("height");
+  //slider
+  document.querySelector(".slider-29").setAttribute("loading", "lazy");
 });
-
-//slider
-document.querySelector('.slider-29').setAttribute('loading', 'lazy');
-
-
-
